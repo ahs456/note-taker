32694b49675f1929444622a0b351b01d7f3811ef
Axh <anikahussain1@outlook.com>
2023-06-27 18:56:33 +0100

    GET REQUEST FOR BACK END

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,8 @@
 const notes = require('express').Router();
 const {v4:uuidv4} = require('uuid');
 const {readFromFile, writeToFile, readAndAppend} = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) => {
+    console.info(`${req.method} requested for the notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
