2b2afdb3c093a89bcd23f81efc4f58b2f3c243dd
Axh <anikahussain1@outlook.com>
2023-06-27 19:41:31 +0100

    listening request edited

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.use('/api/');
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
