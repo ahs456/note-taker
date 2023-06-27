8d8524e4117d80372d1ada80963c525a1d54b191
Axh <anikahussain1@outlook.com>
2023-06-27 19:28:04 +0100

    get request and listen request set

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
-app.use('/api/');
 app.use(express.static('public'));
+app.use('/api/');
 
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
