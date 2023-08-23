046b5b579f6dda29d5781940a1756c6c8e43e555
Axh <anikahussain1@outlook.com>
2023-08-23 19:20:24 +0100

    writetofile defined, api called

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -3,6 +3,14 @@ const util = require('util');
 
 const readFromFile = util.promisify(fs.readFile);
 
+const writeToFile = (file, data) => {
+  fs.writeFile(file, JSON.stringify(data, null, 2), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
+
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
+const api = require('./routes/index');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 app.use(express.static('public'));
-app.use('/api/');
+app.use('/api/', api);
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
